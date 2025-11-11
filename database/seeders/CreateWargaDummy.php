e24b8556653b26c4dddf325810603e39b09af7a9
farrassurya <suryaputra24si@mahasiswa.pcr.ac.id>
2025-11-11 10:50:48 +0700

    Database Seeder

---
+++ database/seeders/CreateWargaDummy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateWargaDummy extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         $faker = Faker::create('id_ID');
+
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('warga')->insert([
+                'no_ktp'        => $faker->numerify('################'), // 16 digit angka acak
+                'nama'          => $faker->name(),
+                'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+                'agama'         => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Budha', 'Konghucu']),
+                'pekerjaan'     => $faker->jobTitle(),
+                'telp'          => $faker->phoneNumber(),
+                'email'         => $faker->unique()->safeEmail(),
+            ]);
+        }
+    }
+}
